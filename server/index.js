ebef5c4a9a2f657c5fe06e24f1b8720f6c0fc528
anthony <luxingchaode@gmail.com>
2024-10-11 03:44:29 +0800

    up

--- server/index.js
+++ server/index.js
@@ -11,8 +11,7 @@ import * as Server from 'socket.io';
 
 const app = express();
 const corsConfig = {
-  // origin: process.env.BASE_URL,
-  origin: "*",
+  origin: process.env.BASE_URL,
   credentials: true,
 };
 const PORT=process.env.PORT || 8000
@@ -31,8 +30,7 @@ const server = app.listen(PORT, () => {
 const io = new Server.Server(server, {
   pingTimeout: 60000,
   cors: {
-    // origin: process.env.BASE_URL,
-    origin: "*",
+    origin: process.env.BASE_URL,
   },
 });
 io.on('connection', (socket) => {
